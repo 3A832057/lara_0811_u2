126f2cf5367c7fef9b98b6c497a2b2f1f8aeedce
3A832057 <qoooppadsl@gmail.com>
2021-11-14 19:59:06 +0800

    將helo路由參數改為可選擇性

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ Route::get('r1',function(){
 Route::get('r2',function(){
     return view('welcome');
 });
-Route::get('hello/{name}',function($name){
-    return 'Hello,'.$name;
-});
-//
-//Route::get('hello/{name?}',function($name='Everybody'){
-//return 'Hello,'.$name;
+//Route::get('hello/{name}',function($name){
+//    return 'Hello,'.$name;
 //});
+//
+Route::get('hello/{name?}',function($name='Everybody'){
+return 'Hello,'.$name;
+});
 //Route::get('hello/{name?}',function($name='Everybody'){
 //    return 'Hello,'.$name;
 //})->name("hello.index");
