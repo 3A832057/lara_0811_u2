fa48cb57b41914c14fd46c789656a49ef13b745c
3A832057 <qoooppadsl@gmail.com>
2021-11-14 20:05:32 +0800

    將hello路由命名為hello.index，且在主頁將hello、hello2、hello3超連結連接上

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
-    return 'welcome';
+    return view('welcome');
 });
 Route::get('r1',function(){
     return redirect('r2');
@@ -26,13 +26,13 @@ Route::get('r2',function(){
 //    return 'Hello,'.$name;
 //});
 //
-Route::get('hello/{name?}',function($name='Everybody'){
-return 'Hello,'.$name;
-});
 //Route::get('hello/{name?}',function($name='Everybody'){
-//    return 'Hello,'.$name;
-//})->name("hello.index");
-//
+//return 'Hello,'.$name;
+//});
+Route::get('hello/{name?}',function($name='Everybody'){
+    return 'Hello,'.$name;
+})->name("hello.index");
+
 //Route::get('dashboard',function(){
 //    return 'dashboard';
 //});
